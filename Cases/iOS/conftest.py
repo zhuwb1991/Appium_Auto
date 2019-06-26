8b64763d46a3ace1dbc99e3241994f49235089ad
wenbo1 <wenbo1@teambition.com>
2019-06-26 15:50:51 +0800

    allure报告添加失败截图

--- Cases/iOS/conftest.py
+++ Cases/iOS/conftest.py
@@ -1,10 +1,22 @@
+import pytest
+import allure
 from appium import webdriver
 from config import Env
 
 
-def driver():
+def init_driver():
     """
     初始化driver, 在每个用例的setup()中创建实例
     :return:
     """
     return webdriver.Remote(Env.appium_url, Env.ios_desc)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    driver = init_driver()
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        f = driver.get_screenshot_as_png()
+        allure.attach(f, '失败截图', allure.attachment_type.PNG)
--- Cases/iOS/test_login.py
+++ Cases/iOS/test_login.py
@@ -1,16 +1,17 @@
 from PO.iOS.LoginPage import LoginPage
 from Public.actions import ElementAction
-from .conftest import driver
+from .conftest import init_driver
 
 
 class TestLogin:
 
     def setup(self):
-        self.driver = driver()
+        self.driver = init_driver()
         self.Action = ElementAction(self.driver)
         self.login_page = LoginPage(self.driver)
 
     def test_login(self):
+        """登录1"""
         self.Action.click(self.login_page.input_account)
 
     def test_login2(self):
